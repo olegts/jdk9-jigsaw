9b6f70d403835bfb5b5f138a2b44745f11ab81c3
Oleg Tsal-Tsalko <oleg.tsalko@gmail.com>
2017-07-07 18:14:49 +0300

    Done 03_MultiModuleCompilation

--- session-1-jigsaw-intro/03_MultiModuleCompilation/multiModCompile.sh
+++ session-1-jigsaw-intro/03_MultiModuleCompilation/multiModCompile.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-set -eu
+#set -eu
 
-source ../../common-functions.sh
+#source ../../common-functions.sh
 
-DESTINATION_FOLDER="mods"
+#DESTINATION_FOLDER="mods"
 
-echo ""
-echo "${info} *** Displaying the contents (source files) of the 'src' folder *** ${normal}"
-runTree src
+#echo ""
+#echo "${info} *** Displaying the contents (source files) of the 'src' folder *** ${normal}"
+#runTree src
 
-echo ""
-echo "${info} *** Compiling both modules into the '$DESTINATION_FOLDER' folder *** ${normal}"
-javac -d $DESTINATION_FOLDER \
+#echo ""
+#echo "${info} *** Compiling both modules into the '$DESTINATION_FOLDER' folder *** ${normal}"
+/Users/oleg/dev/jdk-9.jdk/Contents/Home/bin/javac -d mods \
       --module-source-path src $(find . -name "*.java")
 
 # *************************************************************************************
@@ -22,8 +22,8 @@ javac -d $DESTINATION_FOLDER \
 #
 # *************************************************************************************
 
-echo ""
-echo "${info} *** Displaying the contents (modules) of the 'mods' folder *** ${normal}"
-runTree "$DESTINATION_FOLDER"
+#echo ""
+#echo "${info} *** Displaying the contents (modules) of the 'mods' folder *** ${normal}"
+#unTree "$DESTINATION_FOLDER"
 
 # See ../01_Greetings/compile.sh for explanations to above commands
--- session-1-jigsaw-intro/03_MultiModuleCompilation/run.sh
+++ session-1-jigsaw-intro/03_MultiModuleCompilation/run.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-set -eu
+#set -eu
 
-source ../../common-functions.sh
+#source ../../common-functions.sh
 
-echo ""
-echo "${info} *** Running 'com.greetings.Main' from within the mods folder (module 'com.greetings' is dependent on 'org.astro'). *** ${normal}"
-echo 
-java --module-path mods \
-     --module com.greetings/com.greetings.Main
+#echo ""
+#echo "${info} *** Running 'com.greetings.Main' from within the mods folder (module 'com.greetings' is dependent on 'org.astro'). *** ${normal}"
+#echo
+/Users/oleg/dev/jdk-9.jdk/Contents/Home/bin/java \
+     -p mods \
+     -m com.greetings/com.greetings.Main
 
 # See ../01_Greetings/run.sh for explanations to above commands
